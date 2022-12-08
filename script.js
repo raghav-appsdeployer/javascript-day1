531a10febd3d56bb3f46dd64d8b130ad130a2b88
raghav <raghavsharma6996@gmail.com>
2022-12-08 18:58:41 +0530

    adding remaining changes

--- script.js
+++ script.js
@@ -111,4 +111,15 @@ const e = prompt('hindi marks')
 const addition = Number(a)+Number(b)+Number(c)+Number(d)+Number(e)
 const average = addition/5
 console.log("average will be",average)
+if(average<60){
+    console.log('F')
+}else if(average<70){
+    console.log('D')
+}else if(average<80){
+    console.log('C')
+}else if(average<90){
+    console.log('B')
+}else if(average<100){
+    console.log('A')
+}
 }
